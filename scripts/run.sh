b0c3922119cc6f46e9ab8e01c52ec75d0401a315
Martins Eglitis <sitilge@gmail.com>
2017-04-17 22:01:16 +0300

    Rename

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,6 @@
 
 cp /usr/share/edk2.git/ovmf-x64/OVMF_VARS-pure-efi.fd /tmp/OVMF_VARS-pure-efi.fd
 
-QEMU_PA_SAMPLES=128 QEMU_AUDIO_DRV=pa
-
 qemu-system-x86_64 \
   -enable-kvm \
   -m 8196 \
@@ -11,6 +9,7 @@ qemu-system-x86_64 \
   -cpu host,kvm=off \
   -vga none \
   -soundhw hda \
+  -device hda-micro \
   -usb -usbdevice host:046d:c077 -usbdevice host:046d:c31c \
   -device vfio-pci,host=01:00.0,multifunction=on \
   -device vfio-pci,host=01:00.1 \
@@ -18,5 +17,6 @@ qemu-system-x86_64 \
   -drive if=pflash,format=raw,file=/tmp/OVMF_VARS-pure-efi.fd \
   -device virtio-scsi-pci,id=scsi \
   -drive file=./../windows/Win10_1607_N_English_x64.iso,id=isocd,format=raw,if=none -device scsi-cd,drive=isocd \
+  -drive file=/home/martins/Desktop/arch.iso,id=archcd,format=raw,if=none -device ide-cd,bus=ide.1,drive=archcd \
   -drive file=./../images/Win10_1607_N_English_x64.img,id=disk,format=qcow2,if=none,cache=writeback -device scsi-hd,drive=disk \
   -drive file=./../virtio/virtio-win-0.1.130.iso,id=virtiocd,if=none,format=raw -device ide-cd,bus=ide.1,drive=virtiocd
