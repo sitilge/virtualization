f46ab3c6bf81312367762f65aee00e8757e58a09
Martins Eglitis <martins@sitilge.id.lv>
2018-03-15 11:12:31 +0200

    Update the scripts

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-qemu-img create -f qcow2 -o preallocation=metadata,compat=1.1,lazy_refcounts=on ./../images/Win10_1607_N_English_x64.img 64G
+VIRT_DIR=~/Projects/virtualization
+
+qemu-img create \
+	-f qcow2 `#only allocate size when the guest needs it` \
+	-o lazy_refcounts=on `#improve cache speed` \
+	$VIRT_DIR/images/windows10.img 32G
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,24 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-cp /usr/share/edk2.git/ovmf-x64/OVMF_VARS-pure-efi.fd /tmp/OVMF_VARS-pure-efi.fd
-
-QEMU_PA_SAMPLES=128
-QEMU_AUDIO_DRV=alsa
-QEMU_AUDIO_TIMER_PERIOD=0
+VIRT_DIR=~/Projects/virtualization
 
 qemu-system-x86_64 \
-  -enable-kvm \
-  -m 8196 \
-  -smp cores=4,threads=1 \
-  -cpu host,kvm=off \
-  -vga none \
-  -soundhw ac97 \
-  -usb -usbdevice host:046d:c077 -usbdevice host:046d:c31c \
-  -device vfio-pci,host=01:00.0,multifunction=on \
-  -device vfio-pci,host=01:00.1 \
-  -drive if=pflash,format=raw,readonly,file=/usr/share/edk2.git/ovmf-x64/OVMF_CODE-pure-efi.fd \
-  -drive if=pflash,format=raw,file=/tmp/OVMF_VARS-pure-efi.fd \
-  -device virtio-scsi-pci,id=scsi \
-  -drive file=./../windows/Win10_1607_N_English_x64.iso,id=isocd,format=raw,if=none -device scsi-cd,drive=isocd \
-  -drive file=./../images/Win10_1607_N_English_x64.img,id=disk,format=qcow2,if=none,cache=writeback -device scsi-hd,drive=disk \
-  -drive file=./../virtio/virtio-win-0.1.130.iso,id=virtiocd,if=none,format=raw -device ide-cd,bus=ide.1,drive=virtiocd
+	-enable-kvm							`#enable the kvm hypervisor` \
+	-machine type=pc,accel=kvm					`#use kvm on guest` \
+	-m 4096								`#set the memory` \
+	-cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time	`#match cpu to the one of the host, enable hyper-v enlightment for Windows guests` \
+	-vga std							`#std supports up to 2560x1600 without guest drivers` \
+	`#Using virtio instead of IDE to improve the performance. l2_cache_size = disk_size * 8 / cluster_size. The default cluster size is 64K` \
+	-drive file=$VIRT_DIR/images/windows10.img,index=0,media=disk,format=qcow2,l2-cache-size=4M,if=virtio \
+	-drive file=$VIRT_DIR/systems/windows10.iso,index=2,media=cdrom \
+	-drive file=/usr/share/virtio/virtio-win.iso,index=3,media=cdrom
